d556cefeacaba9a8cf94ec53cc1fad65cafb075b
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-09-19 09:11:46 -0400

    solution

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,6 +1,18 @@
 function fibonacci(n) {
   // type your code here
+  let fibo = [0,1];
+  // console.log(fibo[n])
+  while(fibo.length <= n){  
+    let next = fibo[fibo.length -1]+fibo[fibo.length -2]
+    // console.log(next)
+    // console.log(fibo)
+    fibo.push(next)
+    // console.log(fibo)
+  }
+  console.log(fibo[n])
+  return fibo[n]
 }
+
 
 if (require.main === module) {
   // add your own tests in here
